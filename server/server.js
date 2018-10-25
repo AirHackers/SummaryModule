933fa7896332ca9e3a83b983c8593f34228c2ca8
keshavmandayam <keshavmandayam@gmail.com>
2018-10-25 16:47:02 -0700

    made some changes to server logic to handle request params

--- server/server.js
+++ server/server.js
@@ -1,30 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3010
-const bodyParser = require('body-parser')
-const path = require('path')
-const dataGen = require('./dataGen.js').dataGen
-const Home = require('./dataGen.js').home
-const Sequelize = require('sequelize');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const Home = require('./dataGen.js').home;
 
+const app = express();
+const port = 3010;
 
 app.use(bodyParser.json());
 
-app.use('/', express.static(path.join('././client/dist')))
-
-//app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/', express.static(path.join('././client/dist')));
 
 app.get('/home', (req, res) => {
-    Home.findAll({
-        where: {
-          id: Math.floor(Math.random() * (101))
-        }
-    }).then((homes) => {
-        res.send(homes)
-    });
-})
+  let num = Math.floor(Math.random() * 101);
+  if (req.query.home) {
+    num = req.query.home;
+  }
+  Home.findAll({
+    where: {
+      id: num
+    }
+  }).then(homes => {
+    res.send(homes);
+  });
+});
 
 app.listen(port, () => {
-    //dataGen();
-    console.log(`Server listening on port ${port}!`)
-})
+  console.log(`Server listening on port ${port}!`);
+});

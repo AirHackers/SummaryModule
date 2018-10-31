7bd17e64b5c11b432a27e88df77d03be417af5b7
keshavmandayam <keshavmandayam@gmail.com>
2018-10-31 16:48:49 -0700

    completed tests for secondBody component

--- tests/__tests__/secondBodyTest.jsx
+++ tests/__tests__/secondBodyTest.jsx
@@ -5,16 +5,17 @@ import SecondBody from '../../client/src/components/secondBody.jsx';
 test('should have initial button prior to first click', () => {
   var component = shallow(<SecondBody moresummary={'Sample Summary'} />);
   expect(component.find('#Hide').text()).toBe('Read more about the space');
-  //expect(length).toBe(4);
-  //   component.find('#Hide').simulate('click');
-  //   expect(component.find('.clicks-1').length).to.equal(1);
 });
 
 test('should show second button after first click', () => {
   var component = mount(<SecondBody moresummary={'Sample Summary'} />);
   component.find('#Hide').simulate('click');
   expect(component.find('#Hide').text()).toBe('Hide');
-  //expect(length).toBe(4);
-  //   component.find('#Hide').simulate('click');
-  //   expect(component.find('.clicks-1').length).to.equal(1);
+});
+
+test('should show summary data only after initial click', () => {
+  var component = mount(<SecondBody moresummary={'Sample Summary'} />);
+  expect(component.find('#summary').exists()).toBe(false);
+  component.find('#Hide').simulate('click');
+  expect(component.find('#summary').text()).toBe('Sample Summary');
 });

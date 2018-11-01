18aab9732012a62972be89ea4a6c1c17f113b8b6
keshavmandayam <keshavmandayam@gmail.com>
2018-11-01 13:48:06 -0700

    contact fixes

--- client/src/components/contact.jsx
+++ client/src/components/contact.jsx
@@ -18,7 +18,26 @@ function Contact(props) {
         className="modal"
         overlayClassName="overlay"
       >
-        <div>Modal text!</div>
+        <div>
+          <svg
+            viewBox="0 0 24 24"
+            role="img"
+            aria-label="Close"
+            focusable="false"
+            id="sumarymodule_contact_close"
+            onClick={props.handleCloseModal}
+          >
+            <path
+              d="m23.25 24c-.19 0-.38-.07-.53-.22l-10.72-10.72-10.72 10.72c-.29.29-.77.29-1.06 0s-.29-.77 0-1.06l10.72-10.72-10.72-10.72c-.29-.29-.29-.77 0-1.06s.77-.29 1.06 0l10.72 10.72 10.72-10.72c.29-.29.77-.29 1.06 0s .29.77 0 1.06l-10.72 10.72 10.72 10.72c.29.29.29.77 0 1.06-.15.15-.34.22-.53.22"
+              fillRule="evenodd"
+            />
+          </svg>
+          <div>
+            <div>
+              <h2 id="sumarymodule_contact_text">Log in to continue</h2>
+            </div>
+          </div>
+        </div>
       </ReactModal>
     </div>
   );
